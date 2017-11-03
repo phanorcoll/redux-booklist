9085813266c3bc30b632ee8ada31227e5a608c48
Phanor Coll <phanorcoll@gmail.com>
2017-11-03 11:47:17 -0400

    basic working configuration
    this is a initial config for a working reactjs app with ES6, bootstrap

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+import style from './scss/main.scss'
+
+
+
+ReactDom.render( < App / > ,
+    document.getElementById('app')
+)
