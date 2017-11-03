53275edf4337d63bb12afd921f695dbf20d7f6e2
Phanor Coll <phanorcoll@gmail.com>
2017-11-03 16:30:08 -0400

    reducer for the active book
    takes on the action dispatched by selecting a book from the list

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books.jsx';
+import ActiveBook from './reducer_active_book.jsx';
 
 const rootReducer = combineReducers({
-    books: BooksReducer
+    books: BooksReducer,
+    activeBook: ActiveBook
 });
 
 export default rootReducer;
--- app/reducers/reducer_books.jsx
+++ app/reducers/reducer_books.jsx
@@ -1,9 +1,9 @@
 export default () => {
     return [
-        { title: 'Javascript the good parts' },
-        { title: 'Harry Potter' },
-        { title: 'the dark tower' },
-        { title: 'eloquent ruby' },
-        { title: 'Go' }
+        { title: 'Javascript the good parts', pages:101, info:'laskdjflje owjefw f' },
+        { title: 'Harry Potter',pages:39, info:'laskdjflje ' },
+        { title: 'the dark tower',pages:85, info:'lfw f' },
+        { title: 'eloquent ruby',pages:1, info:'laskdjflje owjefw fafasdfwer wer qwerq2wer' },
+        { title: 'Go',pages:70, info:'lwergsf asdfwe' }
     ]
 }
