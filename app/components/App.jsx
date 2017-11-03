615adcac338bda16a0596d9308162befb97a6ec4
Phanor Coll <phanorcoll@gmail.com>
2017-11-03 16:29:10 -0400

    book detail container

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list.jsx';
+import BookDetail from '../containers/book-detail.jsx';
 export default class App extends Component {
     render() {
         return (
             <div className="container">
-                <BookList />
+                <div className="row">
+                    <BookList />
+                    <BookDetail />
+                </div>
             </div>
         );
     }
